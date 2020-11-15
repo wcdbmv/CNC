034f83fcbd4deafbd46fdf6be992b6912d7795d4
Kerimov Ahmed <kerimov.dev@yandex.ru>
2020-11-15 22:59:18 +0300

    Remove DebuggingServer and PureProxy

--- main.py
+++ main.py
@@ -52,13 +52,6 @@ and if remoteport is not given, then 25 is used.
 #
 #   SMTPServer - the base class for the backend.  Raises NotImplementedError
 #   if you try to use it.
-#
-#   DebuggingServer - simply prints each message it receives on stdout.
-#
-#   PureProxy - Proxies all messages to a real smtpd which does final
-#   delivery.  One known problem with this class is that it doesn't handle
-#   SMTP errors from the backend server at all.  This should be fixed
-#   (contributions are welcome!).
 
 import sys
 import os
@@ -72,7 +65,7 @@ import collections
 from libemail.header_value_parser import get_addr_spec, get_angle_addr
 
 __all__ = [
-    "SMTPChannel", "SMTPServer", "DebuggingServer", "PureProxy",
+    "SMTPChannel", "SMTPServer",
 ]
 
 program = sys.argv[0]
@@ -558,81 +551,6 @@ class SMTPServer(asyncore.dispatcher):
 
         """
         raise NotImplementedError
-
-
-class DebuggingServer(SMTPServer):
-
-    def _print_message_content(self, peer, data):
-        inheaders = 1
-        lines = data.splitlines()
-        for line in lines:
-            # headers first
-            if inheaders and not line:
-                peerheader = 'X-Peer: ' + peer[0]
-                if not isinstance(data, str):
-                    # decoded_data=false; make header match other binary output
-                    peerheader = repr(peerheader.encode('utf-8'))
-                print(peerheader)
-                inheaders = 0
-            if not isinstance(data, str):
-                # Avoid spurious 'str on bytes instance' warning.
-                line = repr(line)
-            print(line)
-
-    def process_message(self, peer, mailfrom, rcpttos, data, **kwargs):
-        print('---------- MESSAGE FOLLOWS ----------')
-        if kwargs:
-            if kwargs.get('mail_options'):
-                print('mail options: %s' % kwargs['mail_options'])
-            if kwargs.get('rcpt_options'):
-                print('rcpt options: %s\n' % kwargs['rcpt_options'])
-        self._print_message_content(peer, data)
-        print('------------ END MESSAGE ------------')
-
-
-class PureProxy(SMTPServer):
-    def __init__(self, *args, **kwargs):
-        if 'enable_SMTPUTF8' in kwargs and kwargs['enable_SMTPUTF8']:
-            raise ValueError("PureProxy does not support SMTPUTF8.")
-        super(PureProxy, self).__init__(*args, **kwargs)
-
-    def process_message(self, peer, mailfrom, rcpttos, data):
-        lines = data.split('\n')
-        # Look for the last header
-        i = 0
-        for line in lines:
-            if not line:
-                break
-            i += 1
-        lines.insert(i, 'X-Peer: %s' % peer[0])
-        data = NEWLINE.join(lines)
-        refused = self._deliver(mailfrom, rcpttos, data)
-        # TBD: what to do with refused addresses?
-        print('we got some refusals:', refused, file=DEBUGSTREAM)
-
-    def _deliver(self, mailfrom, rcpttos, data):
-        import smtplib
-        refused = {}
-        try:
-            s = smtplib.SMTP()
-            s.connect(self._remoteaddr[0], self._remoteaddr[1])
-            try:
-                refused = s.sendmail(mailfrom, rcpttos, data)
-            finally:
-                s.quit()
-        except smtplib.SMTPRecipientsRefused as e:
-            print('got SMTPRecipientsRefused', file=DEBUGSTREAM)
-            refused = e.recipients
-        except (OSError, smtplib.SMTPException) as e:
-            print('got', e.__class__, file=DEBUGSTREAM)
-            # All recipients were refused.  If the exception had an associated
-            # error code, use it.  Otherwise,fake it with a non-triggering
-            # exception code.
-            errcode = getattr(e, 'smtp_code', -1)
-            errmsg = getattr(e, 'smtp_error', 'ignore')
-            for r in rcpttos:
-                refused[r] = (errcode, errmsg)
-        return refused
 
 
 class Options:
