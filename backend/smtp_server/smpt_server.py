ec9e22513565abd4b258f30c061ce47734612bc0
Kerimov Ahmed <kerimov.dev@yandex.ru>
2020-12-24 16:29:00 +0300

    Delete comments

--- backend/smtp_server/smpt_server.py
+++ backend/smtp_server/smpt_server.py
@@ -1,40 +1,4 @@
 #! /usr/bin/env python3
-"""An RFC 5321 smtp server with optional RFC 1870 extensions.
-
-Options:
-
-    --nosetuid
-    -n
-        This program generally tries to setuid `nobody', unless this flag is
-        set.  The setuid call will fail if this program is not run as root (in
-        which case, use this flag).
-
-    --version
-    -V
-        Print the version number and exit.
-
-    --class classname
-    -c classname
-        Use `classname' as the concrete SMTP proxy class.  Uses `PureProxy' by
-        default.
-
-    --size limit
-    -s limit
-        Restrict the total size of the incoming message to "limit" number of
-        bytes via the RFC 1870 SIZE extension.  Defaults to 30000000 bytes.
-
-If localhost is not given then `localhost' is used, and if localport is not
-given then 8025 is used.  If remotehost is not given then `localhost' is used,
-and if remoteport is not given, then 25 is used.
-"""
-
-# Overview:
-#
-# This file implements the minimal SMTP protocol as defined in RFC 5321.  It
-# has:
-#
-#   SMTPServer - the base class for the backend.  Raises NotImplementedError
-#   if you try to use it.
 
 import asyncore
 import socket
@@ -70,9 +34,10 @@ class SmtpServer(asyncore.dispatcher):
             self.close()
             raise
         else:
-            print('%s started at %s\n\tLocal addr: %s\n\tRemote addr:%s' % (
-                self.__class__.__name__, time.ctime(time.time()), local_addr, remote_addr
-            ), file=sys.stderr)
+            print(f'{self.__class__.__name__} started at {time.ctime(time.time())}\n'
+                  f'\tLocal addr: {local_addr}\n'
+                  f'\tRemote addr:{remote_addr}',
+            file=sys.stderr)
 
     def handle_accepted(self, conn, addr):
         print(f'Incoming connection from {repr(addr)}', file=sys.stderr)
@@ -83,34 +48,5 @@ class SmtpServer(asyncore.dispatcher):
                           self._decode_data)
 
     def process_message(self, peer, mailfrom, rcpttos, data, **kwargs):
-        """Override this abstract method to handle messages from the client.
-
-        peer is a tuple containing (ipaddr, port) of the client that made the
-        socket connection to our smtp port.
-
-        mailfrom is the raw address the client claims the message is coming
-        from.
-
-        rcpttos is a list of raw addresses the client wishes to deliver the
-        message to.
-
-        data is a string containing the entire full text of the message,
-        headers (if supplied) and all.  It has been `de-transparencied'
-        according to RFC 821, Section 4.5.2.  In other words, a line
-        containing a `.' followed by other text has had the leading dot
-        removed.
-
-        kwargs is a dictionary containing additional information.  It is
-        empty if decode_data=True was given as init parameter, otherwise
-        it will contain the following keys:
-            'mail_options': list of parameters to the mail command.  All
-                            elements are uppercase strings.  Example:
-                            ['BODY=8BITMIME', 'SMTPUTF8'].
-            'rcpt_options': same, for the rcpt command.
-
-        This function should return None for a normal `250 Ok' response;
-        otherwise, it should return the desired response string in RFC 821
-        format.
-
-        """
+        """Необходимо перегрузить эту функцию"""
         raise NotImplementedError
